4a70cc34044f4bbd40abf556a39edbabfe584603
Ayushi0516 <ayushigupta0516@gmail.com>
2025-12-09 23:34:40 +0530

    landing page joblistingpage

---
+++ src/App.jsx
@@ -0,0 +1,92 @@
+import { useState } from 'react'
+import { Button } from "@/components/ui/button"
+
+import './App.css'
+import AppLayout from './layouts/AppLayout';
+import LandingPage from './pages/LandingPage';
+import { RouterProvider ,createBrowserRouter } from 'react-router-dom';
+import Onboarding from './pages/Onboarding';
+import JobPage from './pages/JobPage';
+import SavedJobs from './pages/SavedJobs';
+import MyJobs from './pages/MyJobs';
+import PostJob from './pages/PostJob';
+import JobListing from './pages/JobListing';
+import { ThemeProvider } from './components/theme-provider';
+import ProtectedRoute from './components/protected-route';
+
+
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <LandingPage />,
+      },
+      {
+        path: "/onboarding",
+        element: (
+          <ProtectedRoute>
+            <Onboarding />
+         </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/jobs",
+        element: (
+          //  <ProtectedRoute>
+            <JobListing />
+          //  </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/post-job",
+        element: (
+           <ProtectedRoute>
+            <PostJob />
+           </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/my-jobs",
+        element: (
+           <ProtectedRoute>
+            <MyJobs />
+           </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/saved-jobs",
+        element: (
+           <ProtectedRoute>
+            <SavedJobs />
+           </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/job/:id",
+        element: (
+           <ProtectedRoute>
+            <JobPage />
+           </ProtectedRoute>
+        ),
+      },
+    ],
+  },
+]);
+
+function App() {
+  
+  return (
+    
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <RouterProvider router={router}/>
+    </ThemeProvider>
+      
+       
+    
+  )
+}
+
+export default App
